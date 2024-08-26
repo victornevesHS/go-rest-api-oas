11400a038b3335ff878db1756037abe14aa5f335
Neves <victorneveshs@gmail.com>
2024-08-25 21:03:00 -0300

    adicionando middleware e integrando com frontend(final)

--- routes/routes.go
+++ routes/routes.go
@@ -4,18 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/victornevesHS/go-rest-api-oas/controllers"
+	"github.com/victornevesHS/go-rest-api-oas/middleware"
 )
 
 func HnadleRequests() {
 	r := mux.NewRouter() //criando instância do gorilla mux
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades", controllers.CriarUmaNovaPersonalidade).Methods("Post")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaUmaPersonalidade).Methods("Delete")
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditarPersonalidade).Methods("Put")
-
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
